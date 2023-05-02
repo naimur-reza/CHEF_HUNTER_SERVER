a66241e7d12095b15fe6c0ad7bb80880955acdf5
Anik <naimur.rezaa@gmail.com>
2023-05-02 14:32:23 +0600

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 5000;
+const chef = require("./data/chef.json");
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chef);
 });
 
 app.listen(port, () => {
